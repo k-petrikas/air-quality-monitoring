6e815a50503e9d494180aea4ab00be7b73294b39
Kevin P <kpetrikas45@gmail.com>
2020-11-29 21:20:18 -0500

    adding base frontend service with rough mockup of list elements to display air quality info

---
+++ frontend/air-quality-monitoring/src/pages/Home.tsx
@@ -0,0 +1,47 @@
+import { IonContent, IonHeader, IonItem, IonLabel, IonList, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import React from 'react';
+import ExploreContainer from '../components/ExploreContainer';
+import './Home.css';
+
+const Home: React.FC = () => {
+
+  const listOfItems = [
+    {
+      "item": "banana"
+    },
+    {
+      "item": "pepper"
+    }]
+
+
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Air Quality Monitoring</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent fullscreen>
+        <IonList>
+          <IonItem>
+            <IonLabel>Pokémon Yellow</IonLabel>
+          </IonItem>
+          <IonItem>
+            <IonLabel>Mega Man X</IonLabel>
+          </IonItem>
+          <IonItem>
+            <IonLabel>The Legend of Zelda</IonLabel>
+          </IonItem>
+          <IonItem>
+            <IonLabel>Pac-Man</IonLabel>
+          </IonItem>
+          <IonItem>
+            <IonLabel>Super Mario World</IonLabel>
+          </IonItem>
+        </IonList>
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Home;
