adf5c63bb5db730bf6ff367ebad16af84635b45e
Kevin P <kpetrikas45@gmail.com>
2020-12-01 20:22:41 -0500

    adding in pre-work for infinite scrolling

--- frontend/air-quality-monitoring/src/pages/Home.tsx
+++ frontend/air-quality-monitoring/src/pages/Home.tsx
@@ -1,17 +1,132 @@
-import { IonContent, IonHeader, IonItem, IonLabel, IonList, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonInfiniteScroll, IonInfiniteScrollContent, IonItem, IonLabel, IonList, IonPage, IonText, IonTitle, IonToolbar } from '@ionic/react';
 import React from 'react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
 
 const Home: React.FC = () => {
 
-  const listOfItems = [
-    {
-      "item": "banana"
-    },
-    {
-      "item": "pepper"
-    }]
+  //dummy list of items 
+  var airQuality =
+    [
+      {
+        "_id": "5fc3e857f6ecf061f0f2abb1",
+        "countryName": "United States",
+        "countryISOCode": "USA",
+        "airQualityRanking": 40,
+        "airQualityValue": 500,
+        "countryPopulation": 330000000,
+        "__v": 0
+      },
+      {
+        "_id": "5fc3e8a5f6ecf061f0f2abb2",
+        "countryName": "United Kingdom",
+        "countryISOCode": "GBR",
+        "airQualityRanking": 30,
+        "airQualityValue": 400,
+        "countryPopulation": 66000000,
+        "__v": 0
+      }
+      ,
+      {
+        "_id": "5fc3e8a5f6ecf061f0f2abb2",
+        "countryName": "United Kingdom",
+        "countryISOCode": "GBR",
+        "airQualityRanking": 30,
+        "airQualityValue": 400,
+        "countryPopulation": 66000000,
+        "__v": 0
+      }
+      ,
+      {
+        "_id": "5fc3e8a5f6ecf061f0f2abb2",
+        "countryName": "United Kingdom",
+        "countryISOCode": "GBR",
+        "airQualityRanking": 30,
+        "airQualityValue": 400,
+        "countryPopulation": 66000000,
+        "__v": 0
+      }
+      ,
+      {
+        "_id": "5fc3e8a5f6ecf061f0f2abb2",
+        "countryName": "United Kingdom",
+        "countryISOCode": "GBR",
+        "airQualityRanking": 30,
+        "airQualityValue": 400,
+        "countryPopulation": 66000000,
+        "__v": 0
+      }
+      ,
+      {
+        "_id": "5fc3e8a5f6ecf061f0f2abb2",
+        "countryName": "United Kingdom",
+        "countryISOCode": "GBR",
+        "airQualityRanking": 30,
+        "airQualityValue": 400,
+        "countryPopulation": 66000000,
+        "__v": 0
+      }
+      ,
+      {
+        "_id": "5fc3e8a5f6ecf061f0f2abb2",
+        "countryName": "United Kingdom",
+        "countryISOCode": "GBR",
+        "airQualityRanking": 30,
+        "airQualityValue": 400,
+        "countryPopulation": 66000000,
+        "__v": 0
+      }
+      ,
+      {
+        "_id": "5fc3e8a5f6ecf061f0f2abb2",
+        "countryName": "United Kingdom",
+        "countryISOCode": "GBR",
+        "airQualityRanking": 30,
+        "airQualityValue": 400,
+        "countryPopulation": 66000000,
+        "__v": 0
+      }
+    ];
+
+  
+
+  async function fetchData() {
+    console.log("fetching data")
+    
+  }
+
+  
+  async function searchNext($event: CustomEvent<void>) {
+    await fetchData();
+
+    ($event.target as HTMLIonInfiniteScrollElement).complete();
+  }
+
+  var airQualityItem = null;
+
+  //used to loop through the air quality data returned from mongo DB
+  airQualityItem = airQuality.map((country) => {
+    return (
+      <IonItem>
+        <IonLabel>country name: {country.countryName}</IonLabel>
+        <IonText >
+          <p>{country.countryPopulation} |</p>
+
+        </IonText>
+        <IonText >
+          <p>{country.airQualityRanking} |</p>
+
+        </IonText>
+        <IonText >
+          <p>{country.airQualityValue} |</p>
+
+        </IonText>
+        <IonText >
+          <p>{country.airQualityValue} </p>
+
+        </IonText>
+      </IonItem>);
+  })
 
 
   return (
@@ -23,25 +138,21 @@ const Home: React.FC = () => {
       </IonHeader>
       <IonContent fullscreen>
         <IonList>
-          <IonItem>
-            <IonLabel>Pokémon Yellow</IonLabel>
-          </IonItem>
-          <IonItem>
-            <IonLabel>Mega Man X</IonLabel>
-          </IonItem>
-          <IonItem>
-            <IonLabel>The Legend of Zelda</IonLabel>
-          </IonItem>
-          <IonItem>
-            <IonLabel>Pac-Man</IonLabel>
-          </IonItem>
-          <IonItem>
-            <IonLabel>Super Mario World</IonLabel>
-          </IonItem>
+          {airQualityItem}
         </IonList>
+        <IonInfiniteScroll id="infinite-scroll" threshold="100px" onIonInfinite={(e: CustomEvent<void>) => searchNext(e)}>
+          <IonInfiniteScrollContent
+            loading-spinner="bubbles"
+            loading-text="Loading more data...">
+          </IonInfiniteScrollContent>
+
+        </IonInfiniteScroll>
       </IonContent>
     </IonPage>
   );
 };
 
 export default Home;
+
+
+
